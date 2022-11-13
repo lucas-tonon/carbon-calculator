40197f102f6349c923f731a500ddf61c423cac1d
lucas-tonon <lucas_tonon@hotmail.com>
2022-11-13 00:18:08 -0300

    Remove unused endpoint

--- server/index.js
+++ server/index.js
@@ -4,12 +4,6 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// app.use(express.static('public'));
-
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
 app.use(require('./routes/electricity'));
 app.use(require('./routes/heating'));
 app.use(require('./routes/transportation'));
